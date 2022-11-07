f6f8d2c156850d934b796b2fb2b992ee4fb43d0a
Aya Gamal <ayag39480@gmail.com>
2022-11-08 00:59:54 +0200

    First commit

---
+++ app/src/main/java/com/aya/noteapp/Note.kt
@@ -0,0 +1,17 @@
+package com.aya.noteapp
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "notesTable")
+
+
+class Note (@ColumnInfo(name = "title")
+val noteTitle :String,@ColumnInfo(name = "description")
+val noteDescription :String,@ColumnInfo(name = "timestamp")
+val timeStamp :String) {
+
+    @PrimaryKey(autoGenerate = true) var id = 0
+}
---
+++ app/src/main/java/com/aya/noteapp/NotesDao.kt
@@ -0,0 +1,22 @@
+package com.aya.noteapp
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface NotesDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note :Note)
+
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("Select * from notesTable order by id ASC")
+    fun getAllNotes(): LiveData<List<Note>>
+
+    @Update
+    suspend fun update(note: Note)
+
+}
